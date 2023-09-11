847c32b8beaa86bb909d92ae92223f698689cf7e
Joel <joelDGJO98@gmail.com>
2023-09-11 13:18:48 -0600

    boton

--- src/app/components/regresar/index.jsx
+++ src/app/components/regresar/index.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Regresar() {
   return (
     <div>
-      <Link className='text-white bg-teal-700 font-bold uppercase text-3xl p-5 border rounded w-60' href="/pantalla-exito" > continuar </Link>
+      <Link className='text-white bg-red-700 font-bold uppercase text-3xl p-5 border rounded w-60' href="/pantalla-exito" > regresar al menu </Link>
     </div>
   );
 }
